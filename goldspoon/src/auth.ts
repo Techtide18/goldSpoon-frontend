6a49933de33323e943223a8cb0425b7942cc8a00
Vineet10pandita <vineet.pandita@birdeye.com>
2024-07-07 15:37:42 +0530

    auth setup

--- goldspoon/src/auth.ts
+++ goldspoon/src/auth.ts
@@ -1,5 +1,47 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Member Id", required: true },
+        password: { label: "Password", type: "password", required: true },
+      },
+      authorize: async (credentials) => {
+        //api hit
+        const user = { id: 100, name: "enz", password: "1234", role: "admin" };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  // pages: {
+  //   signIn: "/login",
+  // },
+  // callbacks: {
+  //   jwt: async ({ token, user }) => {
+  //     if (user) {
+  //       token.role = "admin";
+  //     }
+  //     return token;
+  //   },
+  //   session: async ({ session, token }) => {
+  //     if (session?.user) {
+  //       session.user.role = token.role;
+  //     }
+  //     return session;
+  //   },
+  // },
+  session: {
+    strategy: "jwt",
+    maxAge: 4 * 60,
+  },
 });
